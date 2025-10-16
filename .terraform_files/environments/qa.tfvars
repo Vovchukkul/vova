89c5c588e4e829abecbc53bdeb873bd8f7391b01
Volodymyr Kulivatskyy <kulivackijvoloda@gmail.com>
2025-10-16 11:11:15 +0300

    remove unessesary

--- .terraform_files/environments/qa.tfvars
+++ .terraform_files/environments/qa.tfvars
@@ -1,17 +1,18 @@
 ##### REQUIRED #######
-project_name           = "lasoft"                                         # Name of your project
-region                 = "us-east-1"                                      # Region where resources should be created
-cloudfront_price_class = "PriceClass_All, PriceClass_200, PriceClass_100" # Select one from a list 
-root_domain_name       = "lasoft.org"                                     # Your root domain name, don't use subdomain here
-img_domain_name        = "img.lasoft.org"                                 # Image subdomain
-db_engine_version      = "16.2"
+project_name           = "vova"                                         # Name of your project
+region                 = "eu-west-1"                                      # Region where resources should be created
+cloudfront_price_class = "PriceClass_100" # Select one from a list 
+root_domain_name       = "vova.pp.ua"                                     # Your root domain name, don't use subdomain here
+db_engine_version      = "16.10"
 db_instance_class      = "db.t4g.micro"
 db_storage             = 20
 db_max_storage         = 30
-db_username            = "postgres"
-db_name                = "databasename"
+db_username            = "admin"
+db_name                = "exhibition_db"
+
 app_port               = "8000"
 health_check_url       = "/health-check/"
+
 task_cpu               = 512
 task_memory            = 1024
 task_desired_count     = 1
@@ -23,10 +24,3 @@ vpc_cidr_block             = "10.0.0.0/16"
 subnet_a_cidr_block        = "10.0.1.0/24"
 subnet_b_cidr_block        = "10.0.2.0/24"
 subnet_c_cidr_block        = "10.0.3.0/24"
-db_engine                  = "postgres"
-db_minor_version_upgrade   = true
-db_port                    = "5432"
-redis_engine_version       = "7.0"
-redis_port                 = "6379"
-redis_paramater_group_name = "default.redis7"
-redis_number_of_instances  = 1
--- .terraform_files/main.tf
+++ .terraform_files/main.tf
@@ -90,24 +90,24 @@ module "ecs_fargate" {
   cf_log_group_name   = module.cloud_watch.cf_log_group_name
 }
 
-module "aws_iam" {
-  source       = "./modules/iam"
-  project_name = var.project_name
-  common_tags  = local.common_tags
-  prefix       = local.prefix
-}
+# module "aws_iam" {
+#   source       = "./modules/iam"
+#   project_name = var.project_name
+#   common_tags  = local.common_tags
+#   prefix       = local.prefix
+# }
 
-module "img_bucket" {
-  source           = "./modules/img_bucket"
-  common_tags      = local.common_tags
-  prefix           = local.prefix
-  root_domain_name = var.root_domain_name
-  img_domain_name  = var.img_domain_name
-  region           = var.region
-  config_file_path = local.config_file_path
-  project_name     = var.project_name
+# module "img_bucket" {
+#   source           = "./modules/img_bucket"
+#   common_tags      = local.common_tags
+#   prefix           = local.prefix
+#   root_domain_name = var.root_domain_name
+#   img_domain_name  = var.img_domain_name
+#   region           = var.region
+#   config_file_path = local.config_file_path
+#   project_name     = var.project_name
 
-}
+# }
 
 module "load_balancer" {
   depends_on             = [module.certificate]
@@ -165,36 +165,36 @@ module "vpc" {
   subnet_c_cidr_block = var.subnet_c_cidr_block
 }
 
-module "waf" {
-  source      = "./modules/waf"
-  common_tags = local.common_tags
-  prefix      = local.prefix
-  aws_lb_arn  = module.load_balancer.aws_lb_arn
-}
+# module "waf" {
+#   source      = "./modules/waf"
+#   common_tags = local.common_tags
+#   prefix      = local.prefix
+#   aws_lb_arn  = module.load_balancer.aws_lb_arn
+# }
 
-module "redis" {
-  source       = "./modules/redis"
-  project_name = var.project_name
-  common_tags  = local.common_tags
-  prefix       = local.prefix
-  vpc_id       = module.vpc.vpc_id
-  subnet_a_id  = module.vpc.subnet_a_id
-  subnet_b_id  = module.vpc.subnet_b_id
-  subnet_c_id  = module.vpc.subnet_c_id
-  ecs_sg       = module.ecs_fargate.ecs_sg
-}
+# module "redis" {
+#   source       = "./modules/redis"
+#   project_name = var.project_name
+#   common_tags  = local.common_tags
+#   prefix       = local.prefix
+#   vpc_id       = module.vpc.vpc_id
+#   subnet_a_id  = module.vpc.subnet_a_id
+#   subnet_b_id  = module.vpc.subnet_b_id
+#   subnet_c_id  = module.vpc.subnet_c_id
+#   ecs_sg       = module.ecs_fargate.ecs_sg
+# }
 
-module "eventbridge" {
-  source = "./modules/eventbridge"
-  prefix = local.prefix
+# module "eventbridge" {
+#   source = "./modules/eventbridge"
+#   prefix = local.prefix
 
-  scheduler_backend_down_state      = var.scheduler_backend_down_state
-  scheduler_backend_down_expression = var.scheduler_backend_down_expression
+#   scheduler_backend_down_state      = var.scheduler_backend_down_state
+#   scheduler_backend_down_expression = var.scheduler_backend_down_expression
 
-  scheduler_backend_desired_count = var.scheduler_backend_desired_count
-  scheduler_backend_up_expression = var.scheduler_backend_up_expression
-  scheduler_backend_up_state      = var.scheduler_backend_up_state
+#   scheduler_backend_desired_count = var.scheduler_backend_desired_count
+#   scheduler_backend_up_expression = var.scheduler_backend_up_expression
+#   scheduler_backend_up_state      = var.scheduler_backend_up_state
 
-  ecs_cluster_name         = module.ecs_fargate.cluster_name
-  ecs_backend_service_name = module.ecs_fargate.ecs_backend_service_name
-}
+#   ecs_cluster_name         = module.ecs_fargate.cluster_name
+#   ecs_backend_service_name = module.ecs_fargate.ecs_backend_service_name
+# }
